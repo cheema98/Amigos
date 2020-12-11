18ad9780e03e197f71fddd8d6d61acf0beacc1c9
Priyadarshana Sarma <bharadwaj.priya14@gmail.com>
2020-12-11 16:19:00 -0500

    update

--- priyadarshana_sarma/js/hotspots.js
+++ priyadarshana_sarma/js/hotspots.js
@@ -1,12 +1,24 @@
 
-/*enlarging the image when zoomed in */
-function zoomIn(x) {
-    let current_width = x.clientWidth;
-     x.style.width = (current_width + 100) + "px";        
-}
+// /*enlarging the image when zoomed in */
+// function zoomIn(x) {
+//     let current_width = x.clientWidth;
+//      x.style.width = (current_width + 100) + "px";        
+// }
 
-/*resizing the image when zoomed out*/
-function zoomOut(x) {
-    let current_width = x.clientWidth;
-    x.style.width = (current_width - 100) + "px";  
-}
+// /*resizing the image when zoomed out*/
+// function zoomOut(x) {
+//     let current_width = x.clientWidth;
+//     x.style.width = (current_width - 100) + "px";  
+// }
+
+$(document).ready(function() {
+	
+	$("#myDiv h2").click(
+		function() {
+			$(this).toggleClass("quote");
+		    $(this).next().slideToggle(1000);
+	    }
+    ); 
+}); 
+
+
