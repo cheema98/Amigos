3a53c64350cf2864e8669f9cc56e02ef8b5bdf8c
PriyadarshanaSarma1 <71048051+PriyadarshanaSarma1@users.noreply.github.com>
2020-12-12 21:22:31 -0500

    Update hotspots.js
    update

--- priyadarshana_sarma/js/hotspots.js
+++ priyadarshana_sarma/js/hotspots.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 	var image, imageCounter = 0, imageCache = [];
 	$("#myDiv img").each(function() {	
-		image = new Image();
+	image = new Image();
         image.src = $(this).attr("src");
         image.title = $(this).attr("alt");
         imageCache[imageCounter] = image;
@@ -12,17 +12,17 @@ $(document).ready(function() {
 	//Creating a slideshow with the fading effect
 	imageCounter = 0;
 	var nextImage;
-    setInterval( function () {
+        setInterval( function () {
         $("#caption").fadeOut(1800); 
        	$("#slide").fadeOut(1800,
-			function() {
-				imageCounter = (imageCounter + 1) % imageCache.length; //Changing to next image
+		function() {
+			imageCounter = (imageCounter + 1) % imageCache.length; //Changing to the next image
 	        	nextImage = imageCache[imageCounter];
 		    	$("#slide").attr("src", nextImage.src).fadeIn(1000);
 			    $("#caption").text(nextImage.title).fadeIn(1000);		
 			}        	
-           );     
-    },	
-    1000);
+           	);     
+    	},	
+    	1000);
 }); 
 
